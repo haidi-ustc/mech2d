7b577edbb4293432c548665ebc779a28ea1528b1
haidi <haidi@hfut.edu.cn>
2021-09-13 18:40:52 +0800

    fix minor bug for alpha-P example

--- examples/alpha-P-R/run.sh
+++ examples/alpha-P-R/run.sh
@@ -7,5 +7,5 @@ m2d init -n 9 -a energy -m 0.02 -b
 m2d run -a energy input.yaml
 
 # calculate the stress strain curve along different direction
-m2d init -n 9 -a stress -r 0.0  0.1 -n 11 -p ssc -d 'xx' 'yy' 'xy' 'bi' -b
+m2d init -a stress -r 0.0  0.1 -n 11 -p ssc -d 'xx' 'yy' 'xy' 'bi' -b
 m2d run -a stress -p ssc  input.yaml
