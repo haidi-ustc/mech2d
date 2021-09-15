71cafee5d84ea897a6f3a80f6ff567c0120cf715
haidi <haidi@hfut.edu.cn>
2021-09-16 00:52:21 +0800

    add sphinx doc

---
+++ doc/conf.py
@@ -0,0 +1,78 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../mech2d'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'mech2d'
+copyright = '2021, haidi Wang'
+author = 'haidi Wang'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [    'recommonmark',
+    "sphinx_rtd_theme",
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'numpydoc',
+    'sphinx.ext.autosummary']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+autodoc_default_flags = ['members']
+autosummary_generate = True
+master_doc = 'index'
+
+def run_apidoc(_):
+    
+    from sphinx.ext.apidoc import main
+    os.system('cp ../README.md .')
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = os.path.join(cur_dir, "..", "mech2d")
+    main(['-M', '--tocfile', 'api', '-H', 'mech2d API', '-o', os.path.join(cur_dir, "api"), module, '--force'])
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
+
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/", None),
+}
+    
+
---
+++ doc/index.rst
@@ -0,0 +1,28 @@
+.. mech2d documentation master file, created by
+   sphinx-quickstart on Wed Sep 15 19:48:29 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to mech2d's documentation!
+==================================
+mech2d is a convenient script that use to calculate the mechanical properties of 2D 
+materials, including Stress-Strain Curve, elastic constants and revalant properties. 
+
+mech2d will generate the deformed structure according to the symmetry of structure 
+and automatically submit the DFT task with error custodian. Once the DFT calculations
+finished, mech2d can also be used to parsing the data and plot the figures.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README
+   api/api
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
